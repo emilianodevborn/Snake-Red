0a0890e4c7b81db97f7ae7474c5ca6c383dbe45c
Cristian Etchebarne <cristian@ip-10-113-1-33.ec2.internal>
2025-02-24 18:10:18 -0300

    Pass players from app

--- client/snake-react/src/game/GameTypes.ts
+++ client/snake-react/src/game/GameTypes.ts
@@ -2,21 +2,41 @@
 export type Coordinate = { x: number; y: number };
 
 export interface Snake {
-    id: string;
-    segments: Coordinate[];
-    direction: Coordinate;
-    color: string;
+  id: string;
+  segments: Coordinate[];
+  direction: Coordinate;
+  color: (typeof AVAILABLE_COLORS)[number];
+  newHead?: Coordinate;
 }
 
 export interface GameState {
-    snakes: Snake[];
-    food: Coordinate[];
-    obstacles: Coordinate[];
-    consumedFood: number;
-    gameOver: boolean;
+  snakes: Snake[];
+  food: Coordinate[];
+  obstacles: Coordinate[];
+  consumedFood: number;
+  gameOver: boolean;
 }
+
+export type Player = {
+  id: string;
+  name: string;
+  color?: (typeof AVAILABLE_COLORS)[number];
+};
 
 // Constantes del juego
 export const CANVAS_WIDTH = 600;
 export const CANVAS_HEIGHT = 400;
 export const GRID_SIZE = 20;
+
+export const AVAILABLE_COLORS = [
+  "green",
+  "blue",
+  "red",
+  "yellow",
+  "purple",
+  "orange",
+  "pink",
+  "brown",
+  "gray",
+  "black",
+] as const;
