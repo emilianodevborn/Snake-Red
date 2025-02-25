25994fac35dce15129add0689c20eb1a71aa1c00
Odilon Ferreira <odilon.aferreira@gmail.com>
2025-02-25 17:02:52 -0300

    create Food type and generating food sprite randomly

--- client/snake-react/src/game/generateFood.ts
+++ client/snake-react/src/game/generateFood.ts
@@ -1,17 +1,14 @@
 // src/game/generateFood.ts
-import {
-  Coordinate,
-  CANVAS_WIDTH,
-  CANVAS_HEIGHT,
-  GRID_SIZE,
-} from "./GameTypes";
+import { CANVAS_WIDTH, CANVAS_HEIGHT, GRID_SIZE, Food } from "./GameTypes";
+import { generateRandomFoodSprite } from "./generateRandomFoodSprite";
 
-export const generateFood = (quantity?: number): Coordinate[] => {
-  const foods = [];
+export const generateFood = (quantity?: number): Food[] => {
+  const foods: Food[] = [];
   for (let i = 0; i < (quantity || 1); i++) {
     const x = Math.floor(Math.random() * (CANVAS_WIDTH / GRID_SIZE));
     const y = Math.floor(Math.random() * (CANVAS_HEIGHT / GRID_SIZE));
-    foods.push({ x, y });
+
+    foods.push({ coordinates: { x, y }, sprite: generateRandomFoodSprite() });
   }
   return foods;
 };
