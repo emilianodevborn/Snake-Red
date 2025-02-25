73971c68b5d31fe1d83205ce3942c29e382168e7
Odilon Ferreira <odilon.aferreira@gmail.com>
2025-02-25 15:30:37 -0300

    generate food depending on players number

--- client/snake-react/src/game/generateFood.ts
+++ client/snake-react/src/game/generateFood.ts
@@ -1,7 +1,17 @@
 // src/game/generateFood.ts
-import { Coordinate, CANVAS_WIDTH, CANVAS_HEIGHT, GRID_SIZE } from "./GameTypes";
+import {
+  Coordinate,
+  CANVAS_WIDTH,
+  CANVAS_HEIGHT,
+  GRID_SIZE,
+} from "./GameTypes";
 
-export const generateFood = (): Coordinate => ({
-    x: Math.floor(Math.random() * (CANVAS_WIDTH / GRID_SIZE)),
-    y: Math.floor(Math.random() * (CANVAS_HEIGHT / GRID_SIZE)),
-});
+export const generateFood = (quantity?: number): Coordinate[] => {
+  const foods = [];
+  for (let i = 0; i < (quantity || 1); i++) {
+    const x = Math.floor(Math.random() * (CANVAS_WIDTH / GRID_SIZE));
+    const y = Math.floor(Math.random() * (CANVAS_HEIGHT / GRID_SIZE));
+    foods.push({ x, y });
+  }
+  return foods;
+};
